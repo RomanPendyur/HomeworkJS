101ad7f367af5a3805cc33709177fcb77e1178bd
RomanPendyur <51956203+RomanPendyur@users.noreply.github.com>
2021-05-05 19:09:15 +0300

    second commit

--- js/firsttask.js
+++ js/firsttask.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let productName = document.querySelector(".first_input__style").value;
+/*let productName = document.querySelector(".first_input__style").value;
 let productQuantity = document.querySelector(".second_input__style").value;
 let productStatus = document.querySelector(".third_input__style").value;*/
 let addProductBtn = document.querySelector(".add_new__style");
@@ -9,16 +9,32 @@ let productlist = document.querySelector(".list__style");
 
 let shoppingList = []
 
-
 function getProduct(){
   return {
-    nameProd: productName, 
-    quantity: productQuantity,
-    status: productStatus 
+    nameProd: document.querySelector(".first_input__style").value, 
+    quantity: document.querySelector(".second_input__style").value,
+    status: document.querySelector(".third_input__style").value 
   }
 }
 
+function addNewProduct(){
+  let newProduct = getProduct()
+  shoppingList.push(newProduct)
+  console.log("shoppingList")
+}
 
+
+function productlistFun (){
 addProductBtn.addEventListener("click", function () 
-{return (productlist.innerHTML = `${shoppingList}`);
+{ 
+  let newProduct = getProduct()
+  shoppingList.push(newProduct)
+  console.log(shoppingList)
 });  
+
+wievListBtn.addEventListener("click", function () 
+{ 
+  productlist.innerHTML = shoppingList
+})
+}
+productlistFun ()
